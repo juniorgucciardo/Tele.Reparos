4694fcd4285414be2f60f4489ecc34e77e9310bc
Junior Gucciardo <juniorgucciardo1@gmail.com>
2021-09-29 16:01:00 -0300

    criacao da entidade Attend

---
+++ app/Http/Controllers/AttendController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attend;
+use Illuminate\Http\Request;
+
+class AttendController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return('its working');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Attend  $attend
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attend $attend)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Attend  $attend
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Attend $attend)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Attend  $attend
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attend $attend)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Attend  $attend
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attend $attend)
+    {
+        //
+    }
+}
